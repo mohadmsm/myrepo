4231480d74074228f0eb4167390bc8a21f90b321
mohammed_home_pc <mohadmnssnm@gmail.com>
2024-11-20 21:10:29 +0000

    oop prog1

--- EEN1035_18/src/Assignement/Client.java
+++ EEN1035_18/src/Assignement/Client.java
@@ -7,21 +7,119 @@
 package Assignement;
 
 import java.net.*;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.*;
 
-public class Client {
+@SuppressWarnings("serial")
+public class Client extends JFrame implements ActionListener, WindowListener,KeyListener{
 	
 	private static int portNumber = 5050;
     private Socket socket = null;
     private ObjectOutputStream os = null;
     private ObjectInputStream is = null;
-
+    private JButton Connect, ChangeName;
+	private JTextField Sensor1,Sensor2,Sensor3;
+	private JLabel CN, temp, Sound, Humidity;
+	private JSlider slider1, slider2, slider3;
+	private String serverIP;
 	// the constructor expects the IP address of the server - the port is fixed
-    public Client(String serverIP) {
-    	if (!connectToServer(serverIP)) {
-    		System.out.println("XX. Failed to open socket connection to: " + serverIP);            
-    	}
+    public Client(String serverIP, String DeviceName) {
+    	this.serverIP = serverIP;
+		JPanel hp = new JPanel();
+		hp.setLayout(new BoxLayout(hp, BoxLayout.Y_AXIS));
+		this.getContentPane().add(hp);
+		JPanel p;
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		CN = new JLabel("Device "+DeviceName);
+		p.add(CN);
+		hp.add(p);
+		// Sensor 1 temperature 
+		temp = new JLabel("Temperature   ");
+		Sensor1 = new JTextField(5);
+		slider1 = new JSlider(-10, 100, 20);
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		p.setBorder(new TitledBorder("Sensor 1"));
+		slider1 = new JSlider(0, 100, 10);
+		slider1.setPaintTicks(true);
+		slider1.setMajorTickSpacing(10);
+		p.add(temp);
+		p.add(Sensor1);
+		p.add(slider1);
+		hp.add(p);
+		// Sensor 2 CO2 Level
+		Sound = new JLabel("Sound Level    ");
+		Sensor2 = new JTextField(5);
+		slider2 = new JSlider(-10, 100, 20);
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		p.setBorder(new TitledBorder("Sensor 2"));
+		slider2 = new JSlider(0, 100, 20);
+		slider2.setPaintTicks(true);
+		slider2.setMajorTickSpacing(10);
+		p.add(Sound);
+		p.add(Sensor2);
+		p.add(slider2);
+		hp.add(p);
+		// Sensor 3 Pressure
+		Humidity = new JLabel("Humidity Level ");
+		Sensor3 = new JTextField(5);
+		slider3 = new JSlider(-10, 100, 20);
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		p.setBorder(new TitledBorder("Sensor 2"));
+		slider3 = new JSlider(0, 100, 20);
+		slider3.setPaintTicks(true);
+		slider3.setMajorTickSpacing(10);
+		p.add(Humidity);	
+		p.add(Sensor3);
+		p.add(slider3);
+		hp.add(p);
+		// buttons 
+		Connect = new JButton("connect");
+		ChangeName = new JButton("Edit");
+		p = new JPanel();
+		p.setLayout(new GridLayout(1,2));
+		p.add(Connect);
+		p.add(ChangeName);
+		hp.add(p);
+		this.pack();
+		this.slider1.addChangeListener(e -> updateText(Sensor1, slider1));
+		this.slider2.addChangeListener(e -> updateText(Sensor2, slider2));
+		this.slider3.addChangeListener(e -> updateText(Sensor3, slider3));
+		this.Connect.addActionListener(this);
+		this.updateText(Sensor1, slider1);
+		this.updateText(Sensor2, slider2);
+		this.updateText(Sensor3, slider3);
+		this.setVisible(true);
+		this.addWindowListener(this);
+		this.Sensor1.addKeyListener(this);
+		this.Sensor2.addKeyListener(this);
+		this.Sensor3.addKeyListener(this);
+    	
     }
+    private void updateText(JTextField sensor, JSlider slider) {
+		sensor.setText(String.valueOf(slider.getValue()));		
+	}
 
     private boolean connectToServer(String serverIP) {
     	try { // open a new socket to the server 
@@ -86,9 +184,10 @@ public class Client {
     public static void main(String args[]) 
     {
     	System.out.println("**. Java Client Application - EE402 OOP Module, DCU");
-    	if(args.length==1){
-    		Client theApp = new Client(args[0]);
-		    theApp.getDate();
+    	if(args.length>=1){
+    
+    		new Client(args[0],args[1]);
+		    //theApp.getDate();
 		}
     	else
     	{
@@ -98,4 +197,67 @@ public class Client {
     	}    
     	System.out.println("**. End of Application.");
     }
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) { System.exit(0);}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(Connect)) {
+			System.out.println("here");
+			if (!this.connectToServer(serverIP)) {
+	    		System.out.println("XX. Failed to open socket connection to: " + serverIP);            
+	    	}
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if(!Character.isDigit(e.getKeyChar())) {
+			e.consume();
+		}	
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			JTextField source = (JTextField) e.getSource();
+			int value = Integer.parseInt(source.getText());		
+			if(value>100) {
+				source.setText("100");
+			}
+			else if(value<0){
+				source.setText("0");
+			}
+			if (source == Sensor1) {
+                slider1.setValue(value);
+            } else if (source == Sensor2) {
+                slider2.setValue(value);
+            } else if (source == Sensor3) {
+                slider3.setValue(value);
+            }
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- EEN1035_18/src/Assignement/Client_Gui.java
+++ EEN1035_18/src/Assignement/Client_Gui.java
@@ -4,44 +4,89 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
-//this is a test
 
 @SuppressWarnings("serial")
-public class Client_Gui extends JFrame implements AdjustmentListener, ActionListener, WindowListener
+public class Client_Gui extends JFrame implements  ActionListener, WindowListener
 {
-	JLabel Title = new JLabel("This is a Client");
-	JButton Connect, ChangeName;
-	
+	private JButton Connect, ChangeName;
+	private JTextField Sensor1,Sensor2,Sensor3;
+	private JLabel CN, temp, Sound, Humidity;
+	private JSlider slider1, slider2, slider3;	
 	public Client_Gui(){
+		String s ="1";
 		JPanel hp = new JPanel();
 		hp.setLayout(new BoxLayout(hp, BoxLayout.Y_AXIS));
 		this.getContentPane().add(hp);
-
-		JPanel p = new JPanel();
-		JSlider s = new JSlider(-10, 100, 20);
+		JPanel p;
 		p = new JPanel();
-		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-		p.setBorder(new TitledBorder("Major Ticks"));
-		s = new JSlider(100, 1000, 400);
-		s.setPaintTicks(true);
-		s.setMajorTickSpacing(100);
-		s.getAccessibleContext().setAccessibleName("Major Ticks");
-		s.getAccessibleContext().setAccessibleDescription("A slider showing major tick marks");
-		//s.addChangeListener(this);
-		p.add(s);
+		p.setLayout(new FlowLayout());
+		CN = new JLabel("Device "+s);
+		p.add(CN);
 		hp.add(p);
-
-
+		// Sensor 1 temperature 
+		temp = new JLabel("Temperature   ");
+		Sensor1 = new JTextField(5);
+		 slider1 = new JSlider(-10, 100, 20);
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		p.setBorder(new TitledBorder("Sensor 1"));
+		slider1 = new JSlider(0, 100, 10);
+		slider1.setPaintTicks(true);
+		slider1.setMajorTickSpacing(10);
+		p.add(temp);
+		p.add(Sensor1);
+		p.add(slider1);
+		hp.add(p);
+		// Sensor 2 CO2 Level
+		Sound = new JLabel("Sound Level    ");
+		Sensor2 = new JTextField(5);
+		slider2 = new JSlider(-10, 100, 20);
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		p.setBorder(new TitledBorder("Sensor 2"));
+		slider2 = new JSlider(0, 100, 20);
+		slider2.setPaintTicks(true);
+		slider2.setMajorTickSpacing(10);
+		p.add(Sound);
+		p.add(Sensor2);
+		p.add(slider2);
+		hp.add(p);
+		// Sensor 3 Pressure
+		Humidity = new JLabel("Humidity Level ");
+		Sensor3 = new JTextField(5);
+		slider3 = new JSlider(-10, 100, 20);
+		p = new JPanel();
+		p.setLayout(new FlowLayout());
+		p.setBorder(new TitledBorder("Sensor 2"));
+		slider3 = new JSlider(0, 100, 20);
+		slider3.setPaintTicks(true);
+		slider3.setMajorTickSpacing(10);
+		p.add(Humidity);	
+		p.add(Sensor3);
+		p.add(slider3);
+		hp.add(p);
+		// buttons 
+		Connect = new JButton("connect");
+		ChangeName = new JButton("Edit");
+		p = new JPanel();
+		p.setLayout(new GridLayout(1,2));
+		p.add(Connect);
+		p.add(ChangeName);
+		hp.add(p);
 		this.pack();
+		this.slider1.addChangeListener(e -> updateText(Sensor1, slider1));
+		this.slider2.addChangeListener(e -> updateText(Sensor2, slider2));
+		this.slider3.addChangeListener(e -> updateText(Sensor3, slider3));
+		this.updateText(Sensor1, slider1);
+		this.updateText(Sensor2, slider2);
+		this.updateText(Sensor3, slider3);
 		this.setVisible(true);
+		this.addWindowListener(this);
+	
 	}
-
 	public void actionPerformed(ActionEvent e){
 	
 	}
-
-
-
 	public static void main(String[] args) {
 		new Client_Gui();
 	}
@@ -57,9 +102,7 @@ public class Client_Gui extends JFrame implements AdjustmentListener, ActionList
 	public void windowIconified(WindowEvent arg0) {}
 	public void windowOpened(WindowEvent arg0) {}
 
-	@Override
-	public void adjustmentValueChanged(AdjustmentEvent e) {
-		// TODO Auto-generated method stub
-		
+	private void updateText(JTextField sensor, JSlider slider) {
+		sensor.setText(String.valueOf(slider.getValue()));		
 	}
 }
